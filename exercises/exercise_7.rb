851ab591b13dcba973a214e1de34ed7694f0eeca
Gerard Compion <gcompion93@gmail.com>
2022-03-07 19:13:28 -0500

    complete exercise 7

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -9,4 +9,24 @@ require_relative './exercise_6'
 puts "Exercise 7"
 puts "----------"
 
-# Your code goes here ...
+puts "Please enter a name for a new store:"
+print "> "
+
+store_entry = $stdin.gets.chomp
+
+@store7 = Store.create(name: "#{store_entry}")
+
+if @store7.valid? == false
+  @store7.errors.full_messages.each { |error| puts "Error: #{error}" }
+end
+
+puts "Please enter a name for a new employee:"
+print "> "
+
+employee_entry = $stdin.gets.chomp
+
+@employee7 = Employee.create(first_name: "#{employee_entry}")
+
+if @employee7.valid? == false
+  @employee7.errors.full_messages.each { |error| puts "Error: #{error}" }
+end
